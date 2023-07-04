95f7bb6c22e1b544b654c6f33f7786c999e056aa
riodeja5 <riodeja.5.shota@gmail.com>
2023-07-04 23:17:26 +0900

    [cooking][front] add Button

--- cooking/front/CookingFront/App.tsx
+++ cooking/front/CookingFront/App.tsx
@@ -1,14 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title ="PUSH ME" ></Button>
+      <Button title ="PUSH ME" ></Button>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
+export default App;
 
 const styles = StyleSheet.create({
   container: {
