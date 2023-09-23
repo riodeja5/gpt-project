71a98e909d474259e324f4667fc6539293f72eb8
riodeja5 <riodeja.5.shota@gmail.com>
2023-09-23 23:09:02 +0900

    [cooking][front] add sample graphql front

---
+++ cooking/front/CookingFront/graphqlExample.js
@@ -0,0 +1,24 @@
+async function getData() {
+    const data = JSON.stringify({
+        query: `{
+            rollDice(numDice: 3, numSides:6)
+        }`,
+    });
+
+    const response = await fetch(
+        'http://localhost:4000/graphql',
+        {
+            method: 'post',
+            mode: 'cors',
+            body: data,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }
+    );
+
+    const json = await response.json().catch(err => {console.error(err)});
+    console.log(json.data);
+}
+
+getData();
